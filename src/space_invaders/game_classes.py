4f6df6a164f1ea023a9146273b9aefedee60b7d0
PistaGyros <tomko.jurcik@gmail.com>
2025-01-04 17:34:19 +0100

    working on main loop

--- src/space_invaders/game_classes.py
+++ src/space_invaders/game_classes.py
@@ -1,29 +1,41 @@
 import tkinter as tk
-from constructs import *
+from structs import *
 
 
 class Player:
-    def __init__(self, image_src: str, position: Vector2, rectangle: Rectangle):
-        self.player_img = tk.PhotoImage(file=image_src)
-        self.position = position
+    def __init__(self, image_src: str, rectangle: Rectangle):
+        self.image = tk.PhotoImage(file=image_src)
         self.rectangle = rectangle
 
     def move_player(self, direction):
-        print(self.position)
-        self.position.x += (5 * direction)
-        if self.position.x > 570 or self.position.x < 0:
-            self.position.x -= (5 * direction)
+        print(self.rectangle)
+        self.rectangle.x1 += (5 * direction)
+        self.rectangle.x2 += (5 * direction)
+        if self.rectangle.x1 > 570 or self.rectangle.x1 < 0:
+            self.rectangle.x1 -= (5 * direction)
+            self.rectangle.x2 -= (5 * direction)
+
+    def draw(self, canvas):
+        canvas.create_image(self.rectangle.x1, self.rectangle.y1, anchor="nw", image=self.image)
 
 
 class Bullet:
-    def __init__(self, image_src: str, position: Vector2, rectangle: Rectangle):
-        self.image_src = image_src
-        self.position = position
+    def __init__(self, image_src: str, rectangle: Rectangle):
+        self.image = tk.PhotoImage(file=image_src)
         self.rectangle = rectangle
+
+    def update(self):
+        self.rectangle.y1 -= 5
+        self.rectangle.y2 -= 5
+
+    def draw(self, canvas):
+        canvas.create_image(self.rectangle.x1, self.rectangle.y1, anchor="nw", image=self.image)
 
 
 class Meteorite:
-    def __init__(self, image_src: str, position: Vector2, rectangle: Rectangle):
-        self.image_src = image_src
-        self.position = position
+    def __init__(self, image_src: str, rectangle: Rectangle):
+        self.image = tk.PhotoImage(file=image_src)
         self.rectangle = rectangle
+
+    def draw(self, canvas):
+        canvas.create_image(self.rectangle.x1, self.rectangle.y1, anchor="nw", image=self.image)
--- src/space_invaders/main.py
+++ src/space_invaders/main.py
@@ -1,6 +1,10 @@
 from game_classes import *
 
 
+bullets = []
+meteorites = []
+
+
 def move_player(event):
     if event.char == "a":
         player.move_player(direction=-1)
@@ -9,7 +13,11 @@ def move_player(event):
 
 
 def shoot_bullet(event):
-    bullet = Bullet(image_src="../media/bullet.gif", position=player.position)
+    print("Shoot")
+    bullet = Bullet(image_src="../media/bullet.gif", rectangle=Rectangle(
+                              player.rectangle.x1 + 10, player.rectangle.y1,
+                              player.rectangle.x2 + 10, player.rectangle.y2))
+    bullets.append(bullet)
 
 
 def close_game(event):
@@ -25,20 +33,37 @@ def create_window():
 
 
 root, canvas = create_window()
-
-player = Player(image_src="../media/player_v2.gif", position=Vector2(285, 500), rectangle=Rectangle(285, 500, 315, 530))
 bg = tk.PhotoImage(file="../media/space_invaders_background.gif")
+
+player = Player(image_src="../media/player_v2.gif", rectangle=Rectangle(285, 500, 315, 530))
+
+# Binds
 canvas.bind_all("a", move_player)
 canvas.bind_all("d", move_player)
 canvas.bind_all("<Escape>", close_game)
 canvas.bind_all("<space>", shoot_bullet)
 
+
+def Update():
+    if len(bullets) != 0:
+        for bullet in bullets:
+            bullet.update()
+            print(bullet.rectangle)
+
+
+def Draw():
+    canvas.create_image(0, 0, anchor="nw", image=bg)
+
+    if len(bullets) != 0:
+        for bullet in bullets:
+            bullet.draw(canvas)
+    player.draw(canvas)
+
+
 run = True
 while run:
-    # prcni pozadie do funkcie hore nie takto na gadza
-    # draw
-    canvas.create_image(0, 0, anchor="nw", image=bg)
-    canvas.create_image(player.position.x, player.position.y, anchor="nw", image=player.player_img)
+    Update()
+    Draw()
     canvas.update()
     canvas.after(10)
 
